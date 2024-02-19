4b948d14bf4091c1d6813a8998484d5751552b70
mickaelaustoni <mickael.austoni@gmail.com>
2024-02-19 14:36:00 +0100

    feat: thumbnail animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,96 @@
-'use client';
+"use client";
 
 import Image from "next/image";
-import { SyntheticEvent, useState } from "react";
+import { useState } from "react";
+import { useSprings, animated } from "@react-spring/web";
 
+const IMAGES = [
+  "/image-1.jpg",
+  "/image-2.jpg",
+  "/image-3.jpg",
+];
 
-export default function Home() {
-  const [backgroundImage, setBackgroundImage] = useState('');
+const IMAGE_GAP = 10;
+const IMAGE_WIDTH = 300;
+const IMAGE_HEIGHT = 200;
 
-  const handleImageLoad = (e: SyntheticEvent<HTMLImageElement, Event>) => {
-    const {src} = e.currentTarget;
-    setBackgroundImage(`url(${src})`);
+const getLeftTo = (index: number, selectedImage: number |null) => {
+  const isSelectedImage = selectedImage === index;
+
+  if(isSelectedImage){
+    return 0
   }
 
+  if(selectedImage === null){
+    if(index === 0){
+      return IMAGE_GAP;
+    }
+    return index * IMAGE_WIDTH + index * IMAGE_GAP;
+  }
+
+  if(index === 0){
+    return IMAGE_GAP;
+  }
+
+  if(index > selectedImage){
+    return (index - 1) * IMAGE_WIDTH + index * IMAGE_GAP;
+  }
+
+  return index * IMAGE_WIDTH + index * IMAGE_GAP;
+}
+
+const getLeftFrom = (index: number, selectedImage: number |null) => {
+  if(index === 0){
+    return IMAGE_GAP;
+  }
+
+
+  return IMAGE_GAP + (index * IMAGE_WIDTH + index * IMAGE_GAP);
+
+}
+
+export default function Home() {
+  const [selectedImage, setSelectedImage] = useState<number | null>(null);
+
+  const springs = useSprings(
+    IMAGES.length,
+    IMAGES.map((_, index) => ({
+      from: {
+        width: IMAGE_WIDTH,
+        height: IMAGE_HEIGHT,
+        left: getLeftFrom(index, selectedImage),
+        zIndex: selectedImage === index ? 0 : 1,
+        bottom: IMAGE_GAP,
+      },
+      to: {
+        width: selectedImage === index ? window.innerWidth : IMAGE_WIDTH,
+        height: selectedImage === index ? window.innerHeight : IMAGE_HEIGHT,
+        left: getLeftTo(index, selectedImage),
+        zIndex: selectedImage === index ? 0 : 1,
+        bottom: selectedImage === index ? 0 : IMAGE_GAP,
+      },
+    }))
+  );
+
+  const handleClick = (index: number) => ()=> {
+    setSelectedImage(index);
+  };
+
   return (
-    <main>
-      <ul className="flex flex-row p-10 gap-3 z-10 relative">
-        <li>
+    <main className={"overflow-hidden w-full h-full"}>
+      {springs.map((style, index) => (
+        <animated.div key={index} className={"select-none absolute z-10"} style={style}>
           <Image
-            src="/image-1.jpg"
+            src={IMAGES[index]}
             alt="placeholder"
             priority={true}
             height={1920}
             width={1080}
-            className={"select-none cursor-pointer w-100 h-auto"}
-            onClick={handleImageLoad}
+            className={"cursor-pointer w-full h-full object-cover"}
+            onClick={handleClick(index)}
           />
-        </li>
-        <li>
-          <Image
-            src="/image-2.jpg"
-            alt="placeholder"
-            priority={false}
-            height={1920}
-            width={1080}
-            className={"select-none cursor-pointer w-100 h-auto"}
-            onClick={handleImageLoad}
-          />
-        </li>
-      </ul>
-      <div style={{ backgroundImage  }} className={"absolute inset-0 z-0 bg-cover"}></div>
+        </animated.div>
+      ))}
     </main>
   );
 }
