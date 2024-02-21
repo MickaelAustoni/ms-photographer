b7444223a3cbf7e17954270a83f989de3ec64092
mickaelaustoni <mickael.austoni@gmail.com>
2024-02-21 11:05:46 +0100

    draft

--- src/components/FullScreenGallery.tsx
+++ src/components/FullScreenGallery.tsx
@@ -1,105 +1,77 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { useSprings, animated } from "@react-spring/web";
-import useWindowSize from "@/hooks/useWindowSize";
-
-const ANIMATION_DURATION = 350;
-const IMAGE_GAP = 30;
-const IMAGE_WIDTH = 250;
-const IMAGE_HEIGHT = 150;
-
-const getLeftTo = (index: number, selectedImage: number) => {
-  if (selectedImage === index) {
-    return 0;
-  }
-
-  if (index === 0) {
-    return IMAGE_GAP;
-  }
-
-  if (index > selectedImage) {
-    return (index - 1) * IMAGE_WIDTH + index * IMAGE_GAP;
-  }
-
-  if (index < selectedImage) {
-    return index * IMAGE_WIDTH + (index + 1) * IMAGE_GAP;
-  }
-
-  return index * IMAGE_WIDTH + index * IMAGE_GAP;
-};
-
-const getLeftFrom = (index: number) => {
-  if (index === 0) {
-    return IMAGE_GAP;
-  }
-
-  return IMAGE_GAP + (index * IMAGE_WIDTH + index * IMAGE_GAP);
-};
-
-const getZIndex = (index: number, selectedImage: number, isAnimating: boolean) => {
-  if (selectedImage === index && isAnimating) {
-    return 10;
-  }
-
-  if(selectedImage === index) {
-    return 0
-  }
-
-  return 5;
-}
+import {  useState } from "react";
+import { motion, Variants } from "framer-motion";
 
 interface FullScreenGalleryProps {
   images: string[];
 }
 
+const ANIMATION_DURATION = 2;
+const THUMB_GAP = 30;
+const THUMB_WIDTH = 250;
+const THUMB_HEIGHT = 150;
+
+const variants : Variants = {
+  thumb: {
+    opacity: 1,
+    bottom: THUMB_GAP,
+    width: THUMB_WIDTH,
+    height: THUMB_HEIGHT,
+    zIndex: 5
+  },
+  open: {
+    opacity: 1,
+    width: "100%",
+    height: "100%",
+    zIndex: 0,
+    x: 0,
+    y:0,
+  },
+  close: {
+    opacity: [0, 1],
+    bottom: THUMB_GAP,
+    width: THUMB_WIDTH,
+    height: THUMB_HEIGHT,
+    zIndex: 5
+  },
+}
+
+const getXOpen = (index: number, selectedImage: number) => {
+  if (selectedImage === index) {
+    return 0;
+  }
+
+  if (index === 0) {
+    return THUMB_GAP;
+  }
+
+  if (index > selectedImage) {
+    return (index - 1) * THUMB_WIDTH + index * THUMB_GAP;
+  }
+
+  if (index < selectedImage) {
+    return index * THUMB_WIDTH + (index + 1) * THUMB_GAP;
+  }
+
+  return index * THUMB_WIDTH + index * THUMB_GAP;
+};
+
+const getX = (index: number) => {
+  if (index === 0) {
+    return THUMB_GAP;
+  }
+
+  return THUMB_GAP + (index * THUMB_WIDTH + index * THUMB_GAP);
+};
+
 export default function FullScreenGallery({ images }: FullScreenGalleryProps) {
-  const { height, width } = useWindowSize();
   const [selectedImage, setSelectedImage] = useState(0);
   const [lastSelectedImage, setLastSelectedImage] = useState(-1);
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  const springs = useSprings(
-  images.length,
-  images.map((_, index) => {
-    const isSelectedImage = selectedImage === index;
-    const parallaxX = !isAnimating && isSelectedImage && mousePosition.x ? Math.abs((mousePosition.x - width / 2) / 50) : 0;
-    const parallaxY = !isAnimating && isSelectedImage && mousePosition.y ? Math.abs((mousePosition.y - height / 2) / 50) : 0;
-
-    return {
-      onStart: () => {
-        setIsAnimating(true);
-      },
-      config: {
-        duration: ANIMATION_DURATION,
-      },
-      from: {
-        bottom: IMAGE_GAP,
-        left: getLeftFrom(index),
-        width: isSelectedImage ? IMAGE_WIDTH : width,
-        height: isSelectedImage ? IMAGE_HEIGHT : height,
-        transform: `translate3d(${parallaxX}px, ${parallaxY}px, 0)`,
-        zIndex: getZIndex(index, selectedImage, isAnimating),
-      },
-      to: {
-        bottom: isSelectedImage ? 0 : IMAGE_GAP,
-        width: isSelectedImage ? width : IMAGE_WIDTH,
-        height: isSelectedImage ? height : IMAGE_HEIGHT,
-        left: getLeftTo(index, selectedImage),
-        transform: `translate3d(${parallaxX}px, ${parallaxY}px, 0)`,
-        zIndex: getZIndex(index, selectedImage, isAnimating),
-      },
-      onRest: () => {
-        setIsAnimating(false);
-      }
-    };
-  })
-);
 
   const handleClick = (index: number) => () => {
-    if (selectedImage === index || isAnimating) {
+    if (selectedImage === index) {
       return;
     }
 
@@ -107,29 +79,47 @@ export default function FullScreenGallery({ images }: FullScreenGalleryProps) {
     setSelectedImage(index);
   };
 
-  // Parallax effect
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-    };
+  return images.map((src, index) => {
+    const isSelectedImage = selectedImage === index;
 
-    window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
-  return springs.map((style, index) => {
-    const isThumb = selectedImage !== index
-    const fade = isThumb && isAnimating || lastSelectedImage === index && isAnimating;
-
-    return <animated.div
+    return <motion.div
       key={index}
-      style={style}
-      className={`select-none absolute z-10 origin-top transition-opacity ${isThumb ? "thumb" : "full"} ${fade ? "opacity-0 duration-300" : "opacity-1 duration-1000"}`}>
+      className={"select-none absolute z-10"}
+      variants={{
+        thumb: {
+          opacity: 1,
+          bottom: THUMB_GAP,
+          width: THUMB_WIDTH,
+          height: THUMB_HEIGHT,
+          zIndex: 5,
+          x: getXOpen(index, selectedImage)
+        },
+        open: {
+          opacity: 1,
+          width: "100%",
+          height: "100%",
+          zIndex: 0,
+          x: [getX(index), 0],
+          y: [THUMB_GAP, 0],
+        },
+        close: {
+          opacity: [0, 1],
+          bottom: THUMB_GAP,
+          width: THUMB_WIDTH,
+          height: THUMB_HEIGHT,
+          zIndex: 5
+        },
+      }}
+      animate={isSelectedImage ? "open" : "thumb"}
+      transition={{
+        duration : ANIMATION_DURATION
+      }}
+      initial={{
+        opacity: 0,
+      }}
+    >
       <Image
-        src={images[index]}
+        src={src}
         alt="placeholder"
         priority={true}
         height={1920}
@@ -137,6 +127,6 @@ export default function FullScreenGallery({ images }: FullScreenGalleryProps) {
         className={`cursor-pointer w-full h-full object-cover${selectedImage === index ? " scale-105" : ""}`}
         onClick={handleClick(index)}
       />
-    </animated.div>
+    </motion.div>
   });
 }
