a0ce0e4bcfe5e4a1b2731c44cadc51b63914844e
mickaelaustoni <mickael.austoni@gmail.com>
2024-02-24 14:19:43 +0100

    feat: improvement

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <h1 className={"z-20 absolute text-7xl left-6 top-6 tracking-widest select-none"}>
+    <h1 className={"z-50 absolute text-7xl left-6 top-6 tracking-widest select-none"}>
       MICHAEL
       <br/>
       <span className={"text-primary font-thin"}>SANCHEZ</span>
