4c46c62fcba847fba07cb2aad7da779ba317144c
mickaelaustoni <mickael.austoni@gmail.com>
2024-02-27 23:39:29 +0100

    feat: copyright text

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,7 +3,7 @@ const date = new Date().getFullYear()
 export default function Copyright() {
   return (
     <span className={"absolute bottom-3 right-3 text-xs z-50"}>
-      Copyright © {date} Michael Sanchez - Tous droits réservés
+      Copyright © {date} Michael Sanchez
     </span>
   );
 }
