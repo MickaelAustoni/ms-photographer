513e79b4dfe61e163d9699709124a10de0d491ca
mickaelaustoni <mickael.austoni@gmail.com>
2024-03-04 00:32:14 +0100

    feat: copyright title

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,8 +2,8 @@ const date = new Date().getFullYear()
 
 export default function Copyright() {
   return (
-    <span className={"absolute bottom-3 right-3 text-xs z-50"}>
-      Copyright © {date} Michael Sanchez
+    <span className={"absolute bottom-3 right-3 text-xs z-50"} title={"Created by Mickaël Austoni"}>
+      © {date} Michael Sanchez
     </span>
   );
 }
