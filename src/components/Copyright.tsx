a92f1c07b0e1b938acb2ab0ac38859b762fd1658
mickaelaustoni <mickael.austoni@gmail.com>
2024-03-27 22:03:41 +0100

    feat: copyright improvement

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 const currentYear = new Date().getFullYear();
 
 export default function Copyright() {
   return (
-    <span className={"absolute bottom-3 left-3 text-xs z-50"} title={"Created by Mickaël Austoni"}>
+    <motion.span
+      className={"absolute bottom-3 left-1/2 -translate-x-1/2 text-xs z-50"} title={"Created by Mickaël Austoni"}
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 0.7,
+        transition: {
+          delay: 5.5
+        }
+      }}
+    >
       © {currentYear} Michael Sanchez
-    </span>
+    </motion.span>
   );
 }
